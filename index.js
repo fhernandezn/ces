80e6564d7a968b9fab7b18573a8b1e83304cfe5b
Francisco Hernandez <fhernandez@nearsoft.com>
2019-01-29 13:32:08 -0700

    Initial node setup

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const {
+    PORT
+ } = process.env;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
