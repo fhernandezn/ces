39b7622f8cc7c9e5dadf239ed04f4e5b82500f38
Jesus <jesuspinedaq@gmail.com>
2019-02-09 12:41:39 -0700

    Add GET, POST, PUT and DELETE for users

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers').user;
 
-router.get('/', userController.findById);
+router.get('/', userController.list);
+router.get('/:id', userController.findById);
+router.post('/', userController.create);
+router.put('/', userController.update);
+router.delete('/:id', userController.delete);
 
-module.exports = router; 
+module.exports = router;
