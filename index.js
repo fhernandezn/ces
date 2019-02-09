bd11a02eb61bd18ac262bb6f639d118387d6cd63
Jesus <jesuspinedaq@gmail.com>
2019-02-09 13:39:11 -0700

    Use body-parser

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes/index');
 const app = express();
+const bodyParser = require('body-parser')
 
 app.use(cors());
+app.use( bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true})); 
 app.use(routes);
 
 app.listen(PORT, () => {
